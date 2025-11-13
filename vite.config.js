dd15eeab1364cdd76ac9a9affce177233baa7a51
fmg0018 <fmg0018@alu.medac.es>
2025-11-13 15:16:20 +0100

    Docker 43

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    // La propiedad 'base' resuelve el problema de las rutas en Render.
+    // Usará VITE_ASSET_URL si está definida (en producción) o '/' por defecto.
+    base: process.env.VITE_ASSET_URL || '/',
+    
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    // Configuraciones de servidor de desarrollo que puedes necesitar para el entorno local
+    server: {
+        host: '0.0.0.0', // Es bueno para el entorno Docker local
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
